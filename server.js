c7710fe48ff81fc0d2c71dfe1d412f6ea5061944
Leo Cucinell <leocucinell@gmail.com>
2021-07-26 15:51:23 -0700

    initialized npm and set up file structure:

---
+++ server.js
@@ -0,0 +1,22 @@
+/* SECTION: External modules */
+const express = require("express");
+
+
+
+/* SECTION: Internal modules */
+
+
+/* SECTION: Instanced module */
+const app = express();
+
+
+/* SECTION: Server configuration */
+
+
+/* SECTION: Middleware */
+
+
+/* SECTION: Routes */
+
+
+/* SECTION: Server bind */
